795a60dec7a74e27fb1f462bee7b92134d98862c
RAULT <.RAULT@cea.fr>
2025-09-19 16:40:06 +0200

    ajout des fichiers de tests de l'api

--- Tests/test_api.py
+++ Tests/test_api.py
@@ -1,9 +1,16 @@
 """
-Script de test de l'API créée avec FastAPI.
+Script de test de l'API 
+
+Objectif : Tests fonctionnels sur l'api 
+
+    1- test endpoint racine : démarrage de l'api
+    2- test endpoint "/predic" :
+        - format des données d'entrée
+        - entrée individu seul vs multiples
+        - valeur de prediction
+        - valeurs des probabilités
 
 """
-
-import pytest
 from fastapi.testclient import TestClient
 
 # TestClient est une classe de FastAPI qui permet de simuler des requêtes HTTP pour tester les endpoints de l'API
@@ -11,12 +18,67 @@ from fastapi.testclient import TestClient
 
 from api import app  # on importe l'instance de l'API créée dans api.py
 
+
+
 # on crée un client de test pour envoyer des requêtes à l'API
 client = TestClient(app)
-# on crée une fonction de test pour l'endpoint racine
+# on crée des données de test
+
+data_sample = [
+    {
+        "AMT_CREDIT": 200000,
+        "DAYS_BIRTH": -12000,
+        "EXT_SOURCE_2": 0.65,
+        "FLAG_DOCUMENT_3": 1,
+        "INCOME_TOTAL": 180000
+    }
+]
+
+data_mult = [
+    {
+        "AMT_CREDIT": 20000,
+        "DAYS_BIRTH": -100,
+        "EXT_SOURCE_2": 0.65,
+        "FLAG_DOCUMENT_3": 0,
+        "INCOME_TOTAL": 180000
+    },
+    {
+        "AMT_CREDIT": 2000,
+        "DAYS_BIRTH": -12000,
+        "EXT_SOURCE_2": 0.65,
+        "FLAG_DOCUMENT_3": 1,
+        "INCOME_TOTAL": 0
+    }
+]
+
+# 1- Test sur le fonctionnement de l'api
+
 def test_read_root():
     response = client.get("/")  # on envoie une requête GET à l'endpoint racine
     assert response.status_code == 200  # on vérifie que le code de statut est 200 (OK)
     assert response.json() == {"message": "Welcome to the credit scoring API. Use the /predict endpoint to get predictions."}  # on vérifie que la réponse est correcte
 
-# on crée une fonction de test pour l'endpoint de prédiction
+
+# 2- Tests sur l'endpoint de prédiction
+
+def test_endpoint_predict():
+
+    # check si accepte les requêtes post et le format d'entrée json
+    response = client.post("/predict", json=data_sample)
+    assert response.status_code == 200 
+
+    # check si on a tous les outputs
+    pred = response.json() 
+    assert "prediction"  in pred
+    assert "probabilite de non-solvabilite" in pred
+    assert "les 5 features les plus influentes sur le prediction sont " in pred
+
+    # check les types des outputs
+    assert isinstance(pred["prediction"], list)
+    assert isinstance(pred["probability"], list)
+    assert isinstance(pred["top5_shap"], list)
+
+    # check les valeurs des outputs
+    assert len(pred["top5_shap"][0]) == 5  # top 5 features
+    assert pred["prediction"][0] in [0,1]
+    assert 0<= pred["probabilite de non-solvabilite"][0] <=1
--- api.py
+++ api.py
@@ -1,11 +1,14 @@
 """
 Script de création d'une API avec FastAPI.
 
-Objectif : Fournir une interface pour interagir avec un modèle de prédiction 
+Objectif : Fournir une interface pour interagir avec un modèle de prédiction de scoring de crédit.
+    - recevoir des données en entrée (format JSON)
+    - retourner la prédiction, la probabilité associée (format JSON) et les 5 principales features qui ont influencé la prédiction (explainabilité)
 
 Worflow :
 
 - loader le pipeline de prédiction
+- définir l'explainabilité avec SHAP
 - définir l'API avec FastAPI
 - définir les endpoints pour les prédictions
 
@@ -16,6 +19,7 @@ Worflow :
 from fastapi import FastAPI, Request
 import pickle
 import pandas as pd
+import shap
 
 
 
@@ -27,15 +31,11 @@ with open("./Scripts/pipeline_final.pkl", "rb") as f:
     model_pipeline = pickle.load(f)
 
 
-# 3- définiton du schéma des données d'entrée avec Pydantic
-
-"""
-Pydantic est une bibliothèque de validation de données utilisée par FastAPI pour définir et valider les schémas de données d'entrée et de sortie des endpoints.
-Ici, on définit un modèle de données d'entrée en utilisant une classe qui hérite de BaseModel.
-Chaque attribut de la classe correspond à une caractéristique attendue dans les données d'entrée
-"""
 
 
+# 3- définition de l'explainabilité avec SHAP
+
+explainer = shap.TreeExplainer(model_pipeline.named_steps['model'])
 
 
 # 4- définir l'API avec FastAPI
@@ -61,7 +61,7 @@ def read_root():
 # création d'un endpoint de prédiction
 @app.post("/predict")  # endpoint de prédiction : la fonction en dessous sera exécutée lorsqu'une requête POST est envoyée à /predict
 # on prend en entrée de la fonction, les données formatées en JSON (dictionnaire ou liste de dictionnaires)
-async def predict(request : Request): 
+def predict(data : dict): 
     """
     _Summary_ : fonction de prédiction qui reçoit les données en format JSON et retourne la prédiction et la probabilité de solvabilité associée.
     _Arguments_ :
@@ -69,18 +69,28 @@ async def predict(request : Request):
     _Returns_ :
         dict : dictionnaire contenant la prédiction et la probabilité associée
     """
-    data = await request.json()  # récupère le JSON brut
     # 1- conversion du dictionnaire en DataFrame pandas pour pouvoir faire la prédiction
-    # cas 1 : un seul individu = un dictionnaire
-    # cas 2 : plusieurs individus = liste de dictionnaires
+    # un seul individu = un dictionnaire / plusieurs individus = liste de dictionnaires
     input_data = pd.DataFrame (data if isinstance(data, list) else [data])
     
-    # faire la prédiction avec le pipeline chargé
+    # 2- faire la prédiction avec le pipeline chargé
     prediction = model_pipeline.predict(input_data)
-    prediction_proba = model_pipeline.predict_proba(input_data)  # probabilités des classes
-    
+    prediction_proba = model_pipeline.predict_proba(input_data)[:,1]  # probabilité d'être un mauvais payeur (classe 1)
+
+    # 3- explainabilité avec SHAP
+    data_transformed = model_pipeline.named_steps['preprocessor'].transform(input_data)  # on applique le préprocesseur aux données d'entrée
+    shap_values = explainer.shap_values(data_transformed)  # on calcule les valeurs SHAP
+
+    # on affiche les 5 features les plus importantes pour chaque individu
+    explations = []
+    for i in range(len(input_data)):
+        features_shap = dict(zip(input_data.columns, shap_values[i].tolist()))  # on associe chaque feature à sa valeur SHAP
+        top_5_features = sorted(features_shap.items(), key=lambda x: abs(x[1]), reverse=True)[:5]  # on trie les features par valeur absolue de SHAP et on prend les 5 premières
+        explations.append(top_5_features)
+
     # retourner la prédiction et la probabilité associée
     return {
         "prediction": prediction.tolist(),
-        "probability": prediction_proba.tolist()
+        "probabilite d'être non solvable": prediction_proba.tolist(),
+        "les 5 features les plus influentes sur le prediction sont ": explations
     }
